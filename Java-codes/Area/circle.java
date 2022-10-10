10cfe88bc35157f22e4c12cd1ca1c6daf9809e6e
sharmasagar33 <97349373+sharmasagar33@users.noreply.github.com>
2022-10-10 09:23:43 +0545

    Contibution for Hacktoberfest
    📝 Simple Java program to find the area of circle

---
+++ Java-codes/Area/circle.java
@@ -0,0 +1,14 @@
+//  Java program to calculate area of a circle
+
+import java.util.Scanner;
+public class AreaOfCircle {
+     
+    public static void main(String[] args) {
+        System.out.println("Enter radius of circle: ");
+        Scanner sn = new Scanner(System.in);
+        Double radius = sn.nextDouble();
+         
+        Double area = Math.PI * radius * radius;        
+        System.out.println("Area = "+area);
+    }
+}
