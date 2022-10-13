8f6100c318efb59f7524508653b93bdcfc3e9dd6
sharmasagar33 <97349373+sharmasagar33@users.noreply.github.com>
2022-10-13 22:25:37 +0545

    Contribution for Hacktoberfest

---
+++ Java-codes/impotant Programs/Concatenate.java
@@ -0,0 +1,12 @@
+class Concatenate
+{
+ public static void main(String args[]){
+ 
+   String s1="Sachin ";
+   String s2="Tendulkar";
+
+   String s3=s1.concat(s2);
+
+   System.out.println(s3);//Sachin Tendulkar
+  }
+}
