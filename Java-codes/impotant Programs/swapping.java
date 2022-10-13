4324166f48edfea5066ae890838baa9b2f8b8926
sharmasagar33 <97349373+sharmasagar33@users.noreply.github.com>
2022-10-13 22:22:52 +0545

    Contribution for Hacktoberfest
    📝 Program to Swap two numbers

---
+++ Java-codes/impotant Programs/swapping.java
@@ -0,0 +1,24 @@
+public class Swapping {
+
+    public static void main(String[] args) {
+
+        float first = 13.0f, second = 23.4f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        // Value of first is assigned to temporary
+        float temporary = first;
+
+        // Value of second is assigned to first
+        first = second;
+
+        // Value of temporary (which contains the initial value of first) is assigned to second
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
